a31044f290d6b96d9520061840115e4401c33986
rossyrom84 <rosangely.romero@gmail.com>
2021-05-17 13:37:48 +0200

    java

---
+++ src/main/java/org/factoriaf5/store/controllers/store.java
@@ -0,0 +1,11 @@
+package org.factoriaf5.store.controllers;
+
+public class store {
+    public String check;
+
+    public store(String check){
+        this.check = check;
+    }
+
+    public  String getCheck() {return check; }
+}
