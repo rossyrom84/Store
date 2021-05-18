c1a690f0872886f5d2e4b6a21eccf7f3f67bde56
rossyrom84 <rosangely.romero@gmail.com>
2021-05-18 12:59:09 +0200

    quinta fase

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -4,7 +4,7 @@ import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,16 +17,16 @@ public class ProductController {
         return greeting;
     }
 
-    public List<Product> products = new ArrayList<>();
+    private final List<Product> products = new ArrayList<>();
 
     @GetMapping("/products")
-    public List<Product> allproducts() { return  products; }
+    public List<Product> allProducts() {
+        return  products; }
 
     @PostMapping("/products")
-    public RedirectView addNewProduct(Product product) {
+    public void addNewProduct(Product product) {
         products.add(product);
-        return new RedirectView("/products");
     }
-};
+}
 
 
